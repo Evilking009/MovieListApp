fb7388910ed288de27d43838f23f300890fa4745
Muzaffar Hassan <muzaffarhassan64@gmail.com>
2023-10-09 01:21:26 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,19 +8,36 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState('');
   const [animatation, setAnimation] = useState(false);
+  const [emptyText, setEmptyText] = useState('Search Any Movie');
 
   const searchMovies = async (title) => {
+    try{
+    
     const responce = await fetch(`${API_URL}&s=${title}`);
     const data = await responce.json();
+    
+    if(data.Error){
+      setEmptyText(data.Error);
+      setMovies([]);
+      throw Error();
+    }
+    
+    
     setAnimation(true);
     setMovies(data.Search);
     setTimeout(() => setAnimation(false), 2000);
+
+    
+
+    } catch(e){}
   };
+
+
 
   const Empty = () => {
     return (
       <div className="empty">
-        <h2>No Movie Found! </h2>
+        <h2>{ emptyText } </h2>
       </div>
     );
   };
